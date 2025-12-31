66e09a80c3347816669dcc658620a41848d686af
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2025-12-31 18:30:08 +0100

    Update dependencies in setup.py
    Removed 'accelerate' and 'bitsandbytes' from install_requires.

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "transformers>=4.40.1",
-        "accelerate",
-        "bitsandbytes",
         "pandas",
         "tqdm"
     ],
