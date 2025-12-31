fc57438f9dc684fd041c6e0b23ff0aeea35dc2ad
Tobias Schmidt <106234803+Piece-Of-Schmidt@users.noreply.github.com>
2025-12-31 15:18:25 +0100

    Revise setup.py for package metadata and dependencies
    Updated package name, version, author, and description. Modified install requirements and added classifiers.

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nec",
-    version="0.1",
+    name="neclass", 
+    version="0.1.0",
+    author="Tobias Schmidt / TU Dortmund University",
+    description="Named Entity Classification (NECLass) using Fine-Tuned LLMs",
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Piece-Of-Schmidt/NEClass",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.9',
     install_requires=[
-        "transformers",
+        "transformers>=4.40.1",
         "accelerate",
         "bitsandbytes",
-        "unsloth",
+        "pandas",
         "torch",
-        "pandas"
     ],
 )
